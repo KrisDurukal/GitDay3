8d687db470f8212e3186c43804bd58cd970cb9f4
Emin <memindurukal@gmail.com>
2019-02-04 00:23:09 -0500

    LogOut Method Added

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -25,15 +25,15 @@ public class LoginTests
         Assert.assertEquals(driver.getTitle(),"Web Orders");
     }
 
-//    @Test
-//    public void LogOutTest()
-//    {
-//        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
-//        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
-//        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-//        driver.findElement(By.id("ctl00_logout")).click();
-//        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
-//    }
+    @Test
+    public void LogOutTest()
+    {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        driver.findElement(By.id("ctl00_logout")).click();
+        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
+    }
 
     @BeforeMethod
     public void setUp()
