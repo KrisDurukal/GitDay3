2bea58b528bab1f6cc6bfd10c48f1e24b197a06f
Emin <memindurukal@gmail.com>
2019-02-04 00:18:31 -0500

    After Method Added

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -25,6 +25,16 @@ public class LoginTests
         Assert.assertEquals(driver.getTitle(),"Web Orders");
     }
 
+//    @Test
+//    public void LogOutTest()
+//    {
+//        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+//        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+//        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+//        driver.findElement(By.id("ctl00_logout")).click();
+//        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
+//    }
+
     @BeforeMethod
     public void setUp()
     {
@@ -36,7 +46,6 @@ public class LoginTests
     @AfterMethod
     public void Close()
     {
-
         driver.close();
     }
 }
